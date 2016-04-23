1189ecad98e1038313d52b65ffc9def1378e46ff
Peter Fryer-Davis <pfryerdavis@gmail.com>
2016-04-23 08:31:51 -0400

    Initial Commit

---
+++ Humidity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Humidity
+//
+//  Created by Peter Fryer-Davis on 2016-04-23.
+//  Copyright © 2016 Peter Fryer-Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
